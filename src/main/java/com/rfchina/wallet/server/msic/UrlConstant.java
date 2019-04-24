1f810ff5d988dd2e1db79af12b340ed76300c79a
xiejueheng <xiejueheng@126.com>
2019-04-24 15:12:05 +0800

    feat: 添加钱包相关接口

--- src/main/java/com/rfchina/wallet/server/msic/UrlConstant.java
+++ src/main/java/com/rfchina/wallet/server/msic/UrlConstant.java
@@ -19,8 +19,16 @@ public class UrlConstant {
 	 */
 	public static final String CREATE_WALLET = VERSION + "/wallet/create_wallet";
 
+	/**
+	 * 查询用户信息
+	 */
 	public static final String WALLET_USER_LIST = VERSION + "/wallet/user/list";
 
+	/**
+	 * 查询交易流水
+	 */
 	public static final String WALLET_LOG_LIST = VERSION + "/wallet/log/list";
+
+	public static final String WALLET_BANK_CARD_LIST = VERSION + "/wallet/log/list";
 
 }
