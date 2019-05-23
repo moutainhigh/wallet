8706aae131d0cfecd6c17dc0e8ca3040911a6c6e
niezengming <niezengming@rfchina.com>
2019-05-23 16:26:04 +0800

    feat: 更新sdk

--- rfwallet_server_sdk/src/main/java/com/rfchina/wallet/sdk/model/PayStatusResp.java
+++ rfwallet_server_sdk/src/main/java/com/rfchina/wallet/sdk/model/PayStatusResp.java
@@ -25,7 +25,7 @@ public class PayStatusResp  {
     @ApiModelProperty("失败原因")
     private String errMsg ;
 
-    @ApiModelProperty("交易状态。 1: 待发送银行网关，2：银行受理中，3：交易成功，4：交易失败，5：撤销")
+    @ApiModelProperty("交易状态。 1: 待发送银行网关，2：银行受理中，3：交易成功，4：交易失败，5：撤销，6：待处理")
     private Integer status ;
 
     @ApiModelProperty("交易日期")
