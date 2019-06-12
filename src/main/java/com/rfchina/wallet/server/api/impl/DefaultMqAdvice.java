c272719a767f90879fbd9f63ae8a8ba25ec78b87
niezengming <niezengming@rfchina.com>
2019-06-12 14:14:01 +0800

    feat: 增加MQ日志

--- src/main/java/com/rfchina/wallet/server/api/impl/DefaultMqAdvice.java
+++ src/main/java/com/rfchina/wallet/server/api/impl/DefaultMqAdvice.java
@@ -2,6 +2,7 @@ package com.rfchina.wallet.server.api.impl;
 
 import com.rfchina.biztools.mq.AbstractMqAdvice;
 import com.rfchina.biztools.mq.SimpleMqMsg;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.amqp.core.AmqpTemplate;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@Slf4j
 public class DefaultMqAdvice extends AbstractMqAdvice {
 
 	@Value("${mq.exchange_name}")
@@ -21,6 +23,10 @@ public class DefaultMqAdvice extends AbstractMqAdvice {
 
 	@Override
 	public void publish(SimpleMqMsg msg) {
+		if (msg == null) {
+			return;
+		}
+		log.info("发送MQ {}", msg);
 		amqpTemplate.convertAndSend(
 			StringUtils.isBlank(msg.getExchageName()) ? exchangeName : msg.getExchageName(),
 			msg.getRoutingKey(),
