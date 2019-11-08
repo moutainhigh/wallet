85dbb4e1d7f14bc02376760b1a7e1835c221277f
niezengming <niezengming@rfchina.com>
2019-11-08 15:34:07 +0800

    feat: 修改绑卡接口

---
+++ src/test/java/com/rfchina/wallet/server/api/impl/SeniorWalletApiImplTest.java
@@ -0,0 +1,16 @@
+package com.rfchina.wallet.server.api.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeniorWalletApiImplTest {
+
+	@Test
+	public void resetSecurityTel() {
+	}
+
+	@Test
+	public void resetPayPwd() {
+	}
+}
