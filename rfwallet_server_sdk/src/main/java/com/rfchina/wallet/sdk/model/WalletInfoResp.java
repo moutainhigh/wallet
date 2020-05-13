afc223604ca4008faa95a3c13ac1fdc08ab2798c
liuyongxi1 <liuyongxi1@rfchina.com>
2020-05-13 16:13:14 +0800

    feat:1.新增业务sdk

--- rfwallet_server_sdk/src/main/java/com/rfchina/wallet/sdk/model/WalletInfoResp.java
+++ rfwallet_server_sdk/src/main/java/com/rfchina/wallet/sdk/model/WalletInfoResp.java
@@ -2,10 +2,6 @@ package com.rfchina.wallet.sdk.model;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import java.util.List;
-import java.util.Map;
-import java.util.Date;
-import java.util.ArrayList;
 
     
 /**
