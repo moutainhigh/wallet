a8939fa3687e3d6b4c0362a40026b55a097f6477
niezengming <niezengming@rfchina.com>
2020-08-03 18:38:42 +0800

    feat: 优化钱包余额明细导出

--- src/main/java/com/rfchina/wallet/server/api/impl/SeniorOrderApiImpl.java
+++ src/main/java/com/rfchina/wallet/server/api/impl/SeniorOrderApiImpl.java
@@ -4,20 +4,15 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rfchina.passport.token.EnumTokenType;
 import com.rfchina.passport.token.TokenVerify;
-import com.rfchina.platform.biztools.fileserver.EnumFileAcl;
 import com.rfchina.platform.biztools.fileserver.FileServer;
 import com.rfchina.platform.common.annotation.Log;
 import com.rfchina.platform.common.annotation.SignVerify;
 import com.rfchina.platform.common.json.ObjectSetter;
 import com.rfchina.platform.common.misc.SymbolConstant;
 import com.rfchina.platform.common.page.Pagination;
-import com.rfchina.platform.common.utils.JsonUtil;
-import com.rfchina.wallet.domain.misc.EnumDef.DownloadStatus;
 import com.rfchina.wallet.domain.model.WalletOrder;
 import com.rfchina.wallet.server.api.SeniorOrderApi;
 import com.rfchina.wallet.server.mapper.ext.WalletOrderExtDao;
-import com.rfchina.wallet.server.model.ext.ReportDownloadVo;
-import com.rfchina.wallet.server.msic.RedisConstant;
 import com.rfchina.wallet.server.service.WalletOrderService;
 import java.util.Arrays;
 import java.util.Date;
@@ -26,7 +21,6 @@ import java.util.Objects;
 import java.util.TimeZone;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -50,29 +44,12 @@ public class SeniorOrderApiImpl implements SeniorOrderApi {
 	@Log
 	@TokenVerify(verifyAppToken = true, accept = {EnumTokenType.APP_MANAGER})
 	@SignVerify
-	@Async
 	public void exportOrderDetail(String accessToken, Long walletId, Date beginTime, Date endTime,
 		Byte tradeType, Byte status, String fileName, String uniqueCode) {
 
-		String threadName = Thread.currentThread().getName();
-		log.info("线程[{}]正在导出钱包[{}]订单明细报表[{}]", threadName, walletId, fileName);
+		walletOrderService
+			.exportOrderDetail(uniqueCode,fileName, walletId, tradeType, status, beginTime, endTime);
 
-		byte[] bytes = walletOrderService
-			.exportOrderDetail(fileName, walletId, tradeType, status, beginTime, endTime);
-		String fileKey = "wallet/" + walletId + "/" + fileName;
-		fileServer
-			.upload(fileKey, bytes, "application/octet-stream", EnumFileAcl.PUBLIC_READ, null);
-		BoundHashOperations hashOps = redisTemplate
-			.boundHashOps(RedisConstant.PREX_WALLET_REPORT_DOWNLOAD);
-		if (hashOps.hasKey(uniqueCode)) {
-			String val = (String) hashOps.get(uniqueCode);
-			ReportDownloadVo downloadVo = JsonUtil
-				.toObject(val, ReportDownloadVo.class, getObjectMapper());
-			downloadVo.setStatus(DownloadStatus.BUILDED.getValue());
-			downloadVo.setLocation(getFileSrvPrefix() + fileKey);
-			hashOps.put(uniqueCode, JsonUtil.toJSON(downloadVo, getObjectMapper()));
-		}
-		log.info("线程[{}]完成导出钱包[{}]订单明细报表[{}]", threadName, walletId, fileName);
 	}
 
 	@Log
@@ -101,19 +78,5 @@ public class SeniorOrderApiImpl implements SeniorOrderApi {
 			.build();
 	}
 
-	private ObjectSetter<ObjectMapper> getObjectMapper() {
-		return objectMapper -> {
-			objectMapper.setTimeZone(TimeZone.getDefault());
-			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		};
-	}
 
-	private String getFileSrvPrefix() {
-
-		String prefix = (fileServer.getSvrEndpoint().endsWith(SymbolConstant.SYMBOL_SLASH) ?
-			fileServer.getSvrEndpoint()
-			: fileServer.getSvrEndpoint() + SymbolConstant.SYMBOL_SLASH);
-		return prefix + "_f" + SymbolConstant.SYMBOL_SLASH + fileServer.getSrvBucket()
-			+ SymbolConstant.SYMBOL_SLASH;
-	}
 }
--- src/main/java/com/rfchina/wallet/server/service/WalletOrderService.java
+++ src/main/java/com/rfchina/wallet/server/service/WalletOrderService.java
@@ -1,20 +1,31 @@
 package com.rfchina.wallet.server.service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rfchina.biztools.functional.MaxIdIterator;
 import com.rfchina.platform.biztool.excel.ExcelBean;
 import com.rfchina.platform.biztool.excel.ExcelFactory;
+import com.rfchina.platform.biztools.fileserver.EnumFileAcl;
+import com.rfchina.platform.biztools.fileserver.FileServer;
+import com.rfchina.platform.common.json.ObjectSetter;
+import com.rfchina.platform.common.misc.SymbolConstant;
 import com.rfchina.platform.common.utils.BeanUtil;
 import com.rfchina.platform.common.utils.DateUtil;
 import com.rfchina.platform.common.utils.EmailUtil;
+import com.rfchina.platform.common.utils.JsonUtil;
 import com.rfchina.wallet.domain.misc.EnumDef;
+import com.rfchina.wallet.domain.misc.EnumDef.DownloadStatus;
 import com.rfchina.wallet.domain.model.WalletOrder;
 import com.rfchina.wallet.server.mapper.ext.WalletOrderExtDao;
+import com.rfchina.wallet.server.model.ext.ReportDownloadVo;
 import com.rfchina.wallet.server.model.ext.StatChargingDetailVo;
 import com.rfchina.wallet.server.model.ext.WalletOrderExcelVo;
+import com.rfchina.wallet.server.msic.RedisConstant;
 import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +33,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -41,6 +55,12 @@ public class WalletOrderService {
 	private ConfigService configService;
 	@Autowired
 	private JavaMailSender javaMailSender;
+
+	@Autowired
+	private FileServer fileServer;
+
+	@Autowired
+	private RedisTemplate redisTemplate;
 
 
 	/**
@@ -134,8 +154,12 @@ public class WalletOrderService {
 		return name;
 	}
 
-	public byte[] exportOrderDetail(String fileName, Long walletId, Byte tradeType, Byte status,
-		Date beginTime, Date endTime) {
+	@Async
+	public void exportOrderDetail(String uniqueCode, String fileName, Long walletId,
+		Byte tradeType, Byte status, Date beginTime, Date endTime) {
+
+		String threadName = Thread.currentThread().getName();
+		log.info("线程[{}]正在导出钱包[{}]订单明细报表[{}]", threadName, walletId, fileName);
 
 		ExcelBean excelBean = ExcelFactory.build2007();
 		Sheet sheet = excelBean.creatSheet(fileName);
@@ -162,10 +186,40 @@ public class WalletOrderService {
 			ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
 			excelBean.getWorkbook().write(byteOut);
 
-			return byteOut.toByteArray();
+			String fileKey = "wallet/" + walletId + "/" + fileName;
+			fileServer
+				.upload(fileKey, byteOut.toByteArray(), "application/octet-stream",
+					EnumFileAcl.PUBLIC_READ, null);
+			BoundHashOperations hashOps = redisTemplate
+				.boundHashOps(RedisConstant.PREX_WALLET_REPORT_DOWNLOAD);
+			if (hashOps.hasKey(uniqueCode)) {
+				String val = (String) hashOps.get(uniqueCode);
+				ReportDownloadVo downloadVo = JsonUtil
+					.toObject(val, ReportDownloadVo.class, getObjectMapper());
+				downloadVo.setStatus(DownloadStatus.BUILDED.getValue());
+				downloadVo.setLocation(getFileSrvPrefix() + fileKey);
+				hashOps.put(uniqueCode, JsonUtil.toJSON(downloadVo, getObjectMapper()));
+			}
 		} catch (Exception e) {
-			return new byte[0];
+			log.error("", e);
 		}
 
+		log.info("线程[{}]完成导出钱包[{}]订单明细报表[{}]", threadName, walletId, fileName);
+	}
+
+	private ObjectSetter<ObjectMapper> getObjectMapper() {
+		return objectMapper -> {
+			objectMapper.setTimeZone(TimeZone.getDefault());
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		};
+	}
+
+	private String getFileSrvPrefix() {
+
+		String prefix = (fileServer.getSvrEndpoint().endsWith(SymbolConstant.SYMBOL_SLASH) ?
+			fileServer.getSvrEndpoint()
+			: fileServer.getSvrEndpoint() + SymbolConstant.SYMBOL_SLASH);
+		return prefix + "_f" + SymbolConstant.SYMBOL_SLASH + fileServer.getSrvBucket()
+			+ SymbolConstant.SYMBOL_SLASH;
 	}
 }
